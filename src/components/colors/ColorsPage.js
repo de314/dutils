fe38a850e191ba98757242952c4e37e4de8eace8
David Esposito <david@espo.coffee>
2017-11-13 23:13:12 -0500

    Initial pass at colors page

--- src/components/colors/ColorsPage.js
+++ src/components/colors/ColorsPage.js
@@ -1,11 +1,38 @@
 import React from 'react'
+import _ from 'lodash'
 
+import { compose, withProps, withState } from 'recompose'
 import { withWrapper } from 'components/hoc'
 
-const ColorsPage = () => (
+import { ChromePicker } from 'react-color'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
+
+import './ColorsPage.css'
+
+const HEX_COLOR_PATTERN = /#[a-fA-F0-9]{3,6}/
+
+const ColorsPage = ({ color, setColor, valid }) => (
   <div className="ColorsPage">
-    <h1>Coming, Soon!</h1>
+    <ChromePicker
+      disableAlpha={true}
+      className="mx-auto"
+      color={color}
+      onChangeComplete={c => setColor(c.hex)}
+    />
+    <div className="center mt3">
+      <CopyToClipboard text={color}>
+        <button className="btn btn-outline blue">
+          <i className="fa fa-copy" /> Copy => {color}
+        </button>
+      </CopyToClipboard>
+    </div>
   </div>
 )
 
-export default withWrapper()(ColorsPage)
+export default compose(
+  withWrapper(),
+  withState('color', 'setColor', '#123abc'),
+  withProps(({ color }) => ({
+    valid: _.isString(color) && color.match(HEX_COLOR_PATTERN),
+  })),
+)(ColorsPage)
